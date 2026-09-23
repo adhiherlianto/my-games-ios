6c1976860c95eaab457449d08dbfd9b8f7609582
Muhammad Adhi Herlianto <herlianto.adhi@gmail.com>
2026-09-23 21:06:16 +0700

    Initial Commit

---
+++ MyGamesUITests/MyGamesUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MyGamesUITestsLaunchTests.swift
+//  MyGamesUITests
+//
+//  Created by Muhammad Adhi on 23/09/26.
+//
+
+import XCTest
+
+final class MyGamesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
